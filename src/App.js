6c7fe5e78c6cf4760d549111d9bfeb56817287d5
lcobell <lcobell@gmail.com>
2022-05-13 14:10:46 -0500

    adding nanoid to generate unique ids for new childeren tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // import logo from "./logo.svg";
 // import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
   function addTask(name) {
-    alert(name);
+    const newTask = { id: "id", name: name, completed: false };
+    setTasks([...tasks, newTask]);
   }
 
-  const taskList = props.tasks.map((task) => (
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form(props) {
+  const [name, setName] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    props.addTask("say hi");
+    props.addTask(name);
+    setName("");
   }
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
@@ -18,6 +26,8 @@ export default function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
